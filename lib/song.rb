1302c11a82199b1a55d4df50f204ca1cdc3d9177
Ted Sazon <tedsazon@gmail.com>
2018-12-30 21:11:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,4 +37,17 @@ class Song
     end
     genre_count
   end
+
+  def self.artist_count
+    arist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else 
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+  
 end
