af65f2b38529eec3b092d951116da59aac4c336a
Ted Sazon <tedsazon@gmail.com>
2018-12-30 20:30:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-    @@genres << genre unless @@genres.include?(genre)
+    @@genres << @genre unless @@genres.include?(genre)
   end
 
   def count
