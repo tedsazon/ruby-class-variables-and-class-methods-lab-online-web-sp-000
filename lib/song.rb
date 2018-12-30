944f1e722b353bcd5c684268e938fd0dd4921222
Ted Sazon <tedsazon@gmail.com>
2018-12-30 20:10:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,13 @@ class Song
 
   @@count = 0
   @@genres = []
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@genres << genre unless @@genres[genre]
   end
 
   def count
