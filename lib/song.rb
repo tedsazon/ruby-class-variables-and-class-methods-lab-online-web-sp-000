57cbd3467884310b96612384daac644811e90c90
Ted Sazon <tedsazon@gmail.com>
2018-12-30 21:17:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,7 @@ class Song
   end
 
   def self.artist_count
-    arist_count = {}
+    artist_count = {}
     @@artists.each do |artist|
       if artist_count[artist]
         artist_count[artist] += 1
