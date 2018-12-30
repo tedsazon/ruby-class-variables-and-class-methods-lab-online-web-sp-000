00a2c868fe8d4c6b18c6b408cb0b50917eaa7e05
Ted Sazon <tedsazon@gmail.com>
2018-12-30 21:11:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,11 +43,11 @@ class Song
     @@artists.each do |artist|
       if artist_count[artist]
         artist_count[artist] += 1
-      else 
+      else
         artist_count[artist] = 1
       end
     end
     artist_count
   end
-  
+
 end
